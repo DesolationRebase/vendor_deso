CandyRoms/vendor_candy

Flash SuperSU (1/2)
Other commit is in build

Change-Id: I2cd50b4afe14c6acf04a2b88c22b7c04b5f5009d

config/common.mk
@@ -100,6 +100,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/candy/prebuilt/common/lib/content-types.properties:system/lib/content-types.properties
 
+# superSU
+PRODUCT_COPY_FILES += \
+    vendor/candy/prebuilt/common/etc/SuperSU.zip:system/addon.d/SuperSU.zip \
+    vendor/candy/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
+
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml

DesolationRebase/vendor_deso

config/common.mk
@@ -46,6 +46,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/deso/prebuilt/common/etc/init.d/50selinuxrelabel:system/etc/init.d/50selinuxrelabel
 
+# superSU
+PRODUCT_COPY_FILES += \
+    vendor/deso/prebuilt/common/etc/SuperSU.zip:system/addon.d/SuperSU.zip \
+    vendor/deso/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
+
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml