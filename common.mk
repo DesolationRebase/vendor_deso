1fc033c830574ed78c5edaa7c85a4d030ebaee1a
Jacob McSwain <jacob.a.mcswain@gmail.com>
2017-03-29 23:56:09 -0500

    Add bootanimation selection logic

--- common.mk
+++ common.mk
@@ -25,10 +25,6 @@ PRODUCT_COPY_FILES += \
     vendor/deso/prebuilt/common/bin/blacklist:system/addon.d/blacklist \
     vendor/deso/prebuilt/common/bin/whitelist:system/addon.d/whitelist \
 
-# Bootanimation
-PRODUCT_COPY_FILES += \
-    vendor/deso/prebuilt/common/media/bootanimation.zip:system/media/bootanimation.zip
-
 # init.d support
 PRODUCT_COPY_FILES += \
     vendor/deso/prebuilt/common/bin/sysinit:system/bin/sysinit \
@@ -155,6 +151,43 @@ PRODUCT_PROPERTY_OVERRIDES += persist.sys.recovery_update=false
 ifneq ($(TARGET_BUILD_VARIANT),userdebug)
 # Enable ADB authentication
 ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
+endif
+
+# Boot animation include
+ifneq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
+
+# determine the smaller dimension
+TARGET_BOOTANIMATION_SIZE := $(shell \
+  if [ $(TARGET_SCREEN_WIDTH) -lt $(TARGET_SCREEN_HEIGHT) ]; then \
+    echo $(TARGET_SCREEN_WIDTH); \
+  else \
+    echo $(TARGET_SCREEN_HEIGHT); \
+  fi )
+
+# get a sorted list of the sizes
+bootanimation_sizes := $(subst .zip,, $(shell ls vendor/deso/prebuilt/common/bootanimation))
+bootanimation_sizes := $(shell echo -e $(subst $(space),'\n',$(bootanimation_sizes)) | sort -rn)
+
+# find the appropriate size and set
+define check_and_set_bootanimation
+$(eval TARGET_BOOTANIMATION_NAME := $(shell \
+  if [ -z "$(TARGET_BOOTANIMATION_NAME)" ]; then
+    if [ $(1) -le $(TARGET_BOOTANIMATION_SIZE) ]; then \
+      echo $(1); \
+      exit 0; \
+    fi;
+  fi;
+  echo $(TARGET_BOOTANIMATION_NAME); ))
+endef
+$(foreach size,$(bootanimation_sizes), $(call check_and_set_bootanimation,$(size)))
+
+ifeq ($(TARGET_BOOTANIMATION_HALF_RES),true)
+PRODUCT_COPY_FILES += \
+    vendor/deso/prebuilt/common/bootanimation/halfres/$(TARGET_BOOTANIMATION_NAME).zip:system/media/bootanimation.zip
+else
+PRODUCT_COPY_FILES += \
+    vendor/deso/prebuilt/common/bootanimation/$(TARGET_BOOTANIMATION_NAME).zip:system/media/bootanimation.zip
+endif
 endif
 
 # Versioning System
