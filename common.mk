1b4f7a64fe6c29f9c6d779146b497ef5f249c929
Snuzzo <jrracinfan@gmail.com>
2017-07-15 14:14:06 -0400

    Revert "Add HAS_DEVICE_CAM flag"
    This reverts commit be07f4f67d4e4a540a03dff3304cdd94363e479e.

--- common.mk
+++ common.mk
@@ -79,11 +79,8 @@ PRODUCT_PACKAGES += \
     NovaLauncher \
     OmniJaws \
     OmniStyle \
+    OpenCamera \
     DesoHeaders
-
-ifneq($(HAS_DEVICE_CAM),true)
-PRODUCT_PACKAGES += OpenCamera
-endif
 
 # Telephony packages
 PRODUCT_PACKAGES += \
