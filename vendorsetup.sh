673f029e0bc61de3dd3ec8a5be03ea0414387e3d
Snuzzo <jrracinfan@gmail.com>
2017-07-15 04:53:17 -0400

    vendorsetup; Add deso-sync functions
    This bring in paths from sync-paths file
    checks your current head/commit sha position and compares it
    to your sync remote then syncs if they are unequal
    similar to a CRC checksum comparison algorithm
    
    ** May expand later by pulling remotes/revisions dynamically from manifests
    ** for now only works with -remote: deso  -branch: n

---
+++ vendorsetup.sh
@@ -0,0 +1,50 @@
+set_stuff_for_environment
+function deso-sync() {
+if [ $ANDROID_BUILD_TOP == null ]; then
+echo ANDROID_BUILD_TOP not defined. Aborting ...
+else
+if [ $PWD != $ANDROID_BUILD_TOP ]; then
+cd $ANDROID_BUILD_TOP
+fi
+for line in $(cat vendor/deso/sync-paths) ; do
+    cd $line
+    repo-compare $line deso n
+    #echo $PWD
+    cd $ANDROID_BUILD_TOP
+done
+fi
+}
+
+function repo-compare() {
+
+BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+
+echo
+echo checking $1
+echo git url = $2
+echo branch = $3
+
+# bash replace - replace @ with :password@ in the GIT URL
+FOO="$(git ls-remote $2 -h $3 2> /dev/null)"
+if [ "$?" != "0" ]; then
+  echo cannot get remote status
+fi
+FOO_ARRAY=($FOO)
+BAR=${FOO_ARRAY[0]}
+echo [$BAR]
+
+LOCALBAR="$(git rev-parse HEAD)"
+echo [$LOCALBAR]
+echo
+
+if [ "$BAR" == "$LOCALBAR" ]; then
+  #read -t10 -n1 -r -p 'Press any key in the next ten seconds...' key
+  echo No changes
+else
+  #read -t10 -n1 -r -p 'Press any key in the next ten seconds...' key
+  #echo pressed $key
+  echo There are changes between local and remote repositories.
+  echo syncing . . .
+  repo sync .
+fi
+}
